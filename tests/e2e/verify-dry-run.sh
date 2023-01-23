7d69d10aa97ebe304b187695155816fba8575823
Dylan700 <dylanwilliams700@gmail.com>
2023-01-24 09:28:59 +1100

    Fix 😱

--- tests/e2e/verify-dry-run.sh
+++ tests/e2e/verify-dry-run.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 # check that no files were uploaded
-echo "checking ./tmp/src"
-ls ./tmp/src
-if scp -i ./key -o "StrictHostKeyChecking no" -P 2222 -r -B username@sftp-server:/config/src ./tmp/src; then
+scp -i ./key -o "StrictHostKeyChecking no" -P 2222 -r -B username@sftp-server:/config/src ./tmp/src
+scp -i ./key -o "StrictHostKeyChecking no" -P 2222 -r -B username@sftp-server:/config/dist ./tmp/dist;
+
+if ![ -z "$(ls -A ./tmp/src)" ]; then
 	echo "found src directory uploaded when it shouldn't exist!"
-	ls ./tmp/src
 	exit 1
 fi
 
-if scp -i ./key -o "StrictHostKeyChecking no" -P 2222 -r -B username@sftp-server:/config/dist ./tmp/dist;  then
+if ![ -z "$(ls -A ./tmp/src)" ]; then
 	echo "found dist directory uploaded when it shouldn't exist!"
 	exit 1
 fi
